d801b3af66f4d126c269a37ce1b6893edb2a7fc4
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-20 16:41:00 +0100

    chore(assets): upload images to lib

--- packages/assets/src/img/index.ts
+++ packages/assets/src/img/index.ts
@@ -1,4 +1,15 @@
 import portrait from './portrait.webp';
+import screen_1 from './screen_1.png';
+import screen_2 from './screen_2.png';
+import screen_3 from './screen_3.png';
+import screen_4 from './screen_4.png';
+import screen_5 from './screen_5.png';
 
 export { portrait };
+export { screen_1 };
+export { screen_2 };
+export { screen_3 };
+export { screen_4 };
+export { screen_5 };
+
 export * from '.';
