b0ec20bd5c78fe4a406c185ffbf1364171d3a02a
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-20 17:32:08 +0100

    fix(tile): remove unwanted string chunk

--- apps/web/ui/tile/Tile.tsx
+++ apps/web/ui/tile/Tile.tsx
@@ -45,7 +45,7 @@ export const Tile = ({ title, images, description, links }: TileProps) => (
     <ul className="list-none pl-0">
       {links.map((link) => (
         <li key={link.id}>
-          {link.prefix}:{" "}
+          {link.prefix}
           <a
             className="text-blue-700"
             href={link.url}
